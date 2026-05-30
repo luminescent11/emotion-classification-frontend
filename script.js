23265303410af22d6390bb06d3fec0b920720cd6
Rafa deGoma <rafa8ramen@gmail.com>
2026-05-30 16:32:05 -0700

    Initial working frontend with basic radar chart

---
+++ script.js
@@ -0,0 +1,91 @@
+const emotions = {
+    "Angry": 0,
+    "Disgusted": 0,
+    "Fearful": 0,
+    "Happy": 0,
+    "Neutral": 0,
+    "Sad": 0,
+    "Surprised": 0
+}
+
+const chart = new Chart(document.getElementById('radarChart'), {
+    type: 'radar',
+    data: {
+        labels: Object.keys(emotions),
+        datasets: [{
+        data: Object.values(emotions),
+        borderColor: '#1D9E75',
+        backgroundColor: 'rgba(29, 158, 117, 0.15)',
+        pointBackgroundColor: '#1D9E75',
+        }]
+    },
+    options: {
+        scales: {
+        r: {
+            min: 0,
+            max: 1,
+            ticks: { display: false },
+        }
+        },
+        plugins: { legend: { display: false } }
+    }
+})
+
+/* This is the API/server interaction logic */
+
+let API_URL = "https://emotions-api-1043697508655.us-central1.run.app"
+
+let labelsButton = document.getElementById("retrieve-labels-button")
+let submitAudioButton = document.getElementById("submit-audio-button")
+
+let fileInput = document.getElementById("fileInput")
+
+async function uploadFile() {
+    const file = fileInput.files[0]
+    // check the file is valid
+    if (!file) {
+        alert("Please select a file to upload.")
+        return
+    }
+    if (!file.type.startsWith("audio/")) {
+        alert("Please select a valid audio file.")
+        return
+    }
+    if (file.size > 10 * 1024 * 1024) { // 10 MB limit
+        alert("File size exceeds the 10 MB limit.")
+        return
+    }
+    if (fileInput.files.length > 1) {
+        alert("Please select only one file.")
+        return
+    }
+
+    // Create FormData object containing audio file
+    const formData = new FormData()
+    formData.append("file", file)
+
+    const response = await fetch(`${API_URL}/predict`, {
+        method: "POST",
+        body: formData
+    })
+
+    console.log("Predict endpoint URL: ", `${API_URL}/predict`)
+    console.log(response)
+
+    const result = await response.json()
+    document.getElementById("output").textContent = JSON.stringify(result)
+    chart.data.datasets[0].data = Object.values(result)
+    chart.update()
+}
+
+function getLabels() {
+    fetch(`${API_URL}/labels`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // Update the UI with the retrieved labels
+        })
+        .catch(error => {
+            console.error("Error fetching labels:", error);
+        });
+}
